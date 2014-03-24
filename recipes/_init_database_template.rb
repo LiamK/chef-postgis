86bd0f35bd400000ef43dc7170062d7250a00822
Liam Kirsher <liamk@numenet.com>
2014-03-24 13:38:54 -0700

    Re-fix

--- recipes/_init_database_template.rb
+++ recipes/_init_database_template.rb
@@ -1,7 +1,6 @@
 execute 'create_postgis_template' do
   # Use egrep if you want to include the '|'.
-  # Should be only_if, since if grep is successful it returns 0.
-  only_if "psql -qAt --list | egrep -q '^#{node['postgis']['template_name']}\|'", :user => 'postgres'
+  not_if "psql -qAt --list | egrep -q '^#{node['postgis']['template_name']}\|'", :user => 'postgres'
   user 'postgres'
   command <<CMD
 (createdb -E UTF8 --locale=#{node['postgis']['locale']} #{node['postgis']['template_name']} -T template0) &&
